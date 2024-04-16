2d1e23768efe459d8e320da81c1d987a0c3a8fa2
Daniele <Daniele.occhi2000@gmail.com>
2024-04-16 16:32:55 +0200

    RepairAuth

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -23,8 +23,8 @@
         </div>
 
         
-        <div class="mb-3 form-check">
-            <label class="form-label" for="exampleCheck1">Password</label>
+        <div class="mb-3 ">
+            <label for="exampleInputPassword1" class="form-label">Password</label>
             <input name="password" type="password" class="form-control" id="exampleCheck">
           </div>
 
