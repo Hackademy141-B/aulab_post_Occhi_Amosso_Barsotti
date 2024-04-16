da39ec9e37e2644c1d7917871d02058fe6ca2eb0
Daniele <Daniele.occhi2000@gmail.com>
2024-04-16 18:20:35 +0200

    removebg

--- resources/views/article/byCategory.blade.php
+++ resources/views/article/byCategory.blade.php
@@ -1,6 +1,6 @@
 <x-layout>
 
-    <div class="container-fluid p-5 bg-info text-center text-white">
+    <div class="container-fluid p-5  text-center text-white">
         <div class="row justify-content-center">
             <h1 class="display-1 text-capitalize">
                 Categoria {{ $category->name }}
