6eae7d65e336309cbd32adadb293ccd684ec8a84
Daniele <Daniele.occhi2000@gmail.com>
2024-04-16 18:36:28 +0200

    modify card

--- resources/views/article/byCategory.blade.php
+++ resources/views/article/byCategory.blade.php
@@ -12,7 +12,7 @@
         <div class="row justify-content-around">
             @foreach($articles as $article)
                 <div class="col-12 col-md-3 my-2">
-                    <div class="card">
+                    <div class="card shadow p-4 border border-3  rounded-4">
                         <img src="{{ Storage::url($article->img) }}" class="card-img-top" alt="...">
                         <div class="card-body">
                             <h5 class="card-title">{{ $article->title }}</h5>
--- resources/views/article/index.blade.php
+++ resources/views/article/index.blade.php
@@ -10,21 +10,21 @@
     </div>
     
     <div class="container my-5">
-        <div class="row justify-content-around">
+        <div class="row  justify-content-around">
             @foreach($articles as $article)
-                <div class="col-12 col-md-3 my-2">
-                    <div class="card">
+                <div class="col-10 col-md-3 my-2">
+                    <div class="card shadow p-3 border border-3  rounded-4">
                         <img src="{{ Storage::url($article->img) }}" class="card-img-top" alt="...">
                         <div class="card-body">
                             <h5 class="card-title">{{ $article->title }}</h5>
                             <p class="card-text">{{ $article->subtitle }}</p>
-                            <p class="small text-muted fst-italic text-capitalize">{{ $article->category->name }}</p>
+                            <a href="{{ route('article.byCategory', ['category' => $article->category->id]) }}" class="small text-muted  text-capitalize">{{ $article->category->name }}</a>
+                            <a href="{{ route('article.byRedactor', ['user' => $article->user->id]) }}" class="small text-muted  text-capitalize">  {{ $article->user->name }}</a>
+                            <p class="small text-muted  text-capitalize">{{ $article->category->name }}</p>
+                            <div class="text-muted d-none d-flex justify-content-between align-items-center">
+                                Redatto il {{ $article->created_at->format('d/m/Y') }} da {{ $article->user->name }}
                         </div>
-                        <div class="card-footer text-muted d-flex justify-content-between align-items-center">
-                            Redatto il {{ $article->created_at->format('d/m/Y') }} da {{ $article->user->name }}
                             <a href="{{route('article.show', compact('article'))}}" class="btn btn-info text-white">Leggi</a>
-                            <a href="{{ route('article.byCategory', ['category' => $article->category->id]) }}" class="small text-muted fst-italic text-capitalize">{{ $article->category->name }}</a>
-                            <a href="{{ route('article.byRedactor', ['user' => $article->user->id]) }}" class="small text-muted fst-italic text-capitalize">  {{ $article->user->name }}</a>
                         </div>
                     </div>
                 </div>
