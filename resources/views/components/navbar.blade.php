a54a7a8bfe3386922cbb31102189b431dd711740
Daniele <Daniele.occhi2000@gmail.com>
2024-04-18 16:26:49 +0200

    NAvbar

--- resources/views/components/navbar.blade.php
+++ resources/views/components/navbar.blade.php
@@ -1,9 +1,12 @@
 
-
-
-
+{{-- <script type="text/javascript">
+  window.addEventListener("scroll", function(){
+      var header = document.querySelector("navbar");
+      header.classList.toggle("sticky", window.scrollY > 0);
+  }) --}}
+<header>
 <div class="container-fluid">
-      <nav class="navbar navbar-expand-lg bg-body-trasparent navbar-light">
+    <nav class="navbar fixed-top w-100 px-3 navbar-expand-lg bg-body-trasparent  navbar-light">
         <a class="navbar-brand" href="{{ route('homePage') }}">Aulab Post</a>
         <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent"
             aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
@@ -13,44 +16,36 @@
             <ul class="navbar-nav d-flex justify-content-between w-100 mb-2 mb-lg-0">
 
                 <li class="nav-item ">
-                    <a class="nav-link" href="{{route('article.index')}}">Tutti gli articoli</a>
+                    <a class="nav-link" href="{{ route('article.index') }}">Tutti gli articoli</a>
                 </li>
-               
-                {{-- <li class="nav-item dropdown"> --}}
-                  {{-- <a class="nav-link dropdown-toggle" href="#" role="button" data-bs-toggle="dropdown"
-                  aria-expanded="false">
-                  Accedi
-                </a> --}}
-                {{-- <ul class="dropdown-menu">
-                  <li><a class="dropdown-item" href="{{ route('register') }}">Registrati</a></li>
-                  <li><a class="dropdown-item" href="{{ route('login') }}">Accedi</a></li>
-                </ul> --}}
-              {{-- </li> --}}
-            
-              @auth 
-              @if(Auth::user()->is_admin)
-              <li><a class="dropdown-item" href="{{route('admin.dashboard')}}">Dashboard Admin</a></li>
-              @endIf
-              <li class="nav-item ">
-                <a class="nav-link" href="{{route('careers')}}">Lavora con noi</a>
-            </li>
-              <li class="nav-item me-auto">
-                <a class="nav-link" href="{{route('article.create')}}">Inserisci un articolo</a>
-              </li>
-            {{-- </ul>
+                @auth
+                    @if (Auth::user()->is_admin)
+                        <li><a class="dropdown-item" href="{{ route('admin.dashboard') }}">Dashboard Admin</a></li>
+                    @endIf
+                    <li class="nav-item ">
+                        <a class="nav-link" href="{{ route('careers') }}">Lavora con noi</a>
+                    </li>
+                    <li class="nav-item me-auto">
+                        <a class="nav-link" href="{{ route('article.create') }}">Inserisci un articolo</a>
+                    </li>
+                    {{-- </ul>
                   <ul class="navbar-nav"> --}}
-                    <li class="me-3 mt-2">Benvenuto, {{Auth::user()->name}}</li>
-                    <li><a  type="button" class="btn btn-outline-dark mt-auto"  href="#" onclick="event.preventDefault();document.querySelector('#form-logout').submit();">Logout</a></li>
-                    <form id="form-logout" class="d-none" action="{{ route('logout') }}" method="POST" >
-                      @csrf
+                    <li class="me-3 mt-2">Benvenuto, {{ Auth::user()->name }}</li>
+                    <li><a type="button" class="btn btn-outline-dark mt-auto" href="#"
+                            onclick="event.preventDefault();document.querySelector('#form-logout').submit();">Logout</a>
+                    </li>
+                    <form id="form-logout" class="d-none" action="{{ route('logout') }}" method="POST">
+                        @csrf
                     </form>
-                    @else 
-                    <a type="button" class="btn btn-outline-dark" href="{{ route('login') }}"><i class="bi bi-box-arrow-in-right"> </i>Accedi</a> 
-                    @endauth 
-                  </ul >
+                @else
+                    <a type="button" class="btn btn-outline-dark" href="{{ route('login') }}"><i
+                            class="bi bi-box-arrow-in-right"> </i>Accedi</a>
+                @endauth
+            </ul>
         </div>
-    </div>
+</div>
 </nav>
+</header>
 
 {{-- <nav class="navbar bg-body-trasparent fixed-top">
     <div class="container-fluid">
