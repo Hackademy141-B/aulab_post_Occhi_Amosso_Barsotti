23e2683b46b59e50b8d375d285e60d869ed4f385
Daniele <Daniele.occhi2000@gmail.com>
2024-04-24 18:24:48 +0200

    Merge

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -79,6 +79,13 @@
                                                 <a href="{{ route('article.byRedactor', ['user' => $article->user->id]) }}"
                                                     class="small text-muted  text-capitalize text-truncate"><b>Redatto da:</b> 
                                                     {{ $article->user->name }}</a>
+                                                {{-- <p class="small text-muted  text-capitalize">
+                                                    {{ $article->category->name }}</p> --}}
+                                                {{-- <div
+                                                    class="text-muted d-none d-flex justify-content-between align-items-center">
+                                                    Redatto il {{ $article->created_at->format('d/m/Y') }} da
+                                                    {{ $article->user->name }}
+                                                </div> --}}
                                                 <br>
                                                 <a href="{{ route('article.show', compact('article')) }}"
                                                     class="btn btn-secondary mt-2">Leggi</a>
@@ -90,10 +97,10 @@
                         </div>
                     </div>
                 </div>
-                <div class="carousel-item active" data-bs-interval="10000">
+                {{-- <div class="carousel-item" data-bs-interval="2000">
                     <div class="cards-wrapper pb-5">
-                        <div class="container my-5">
-                            <div class="row justify-content-center">
+                        <div class="container  my-5">
+                            <div class="row  justify-content-around ">
                                 @foreach ($articles as $article)
                                     <div class="col-10 col-md-3 my-2 ">
                                         <div class="card  shadow  border border-3  rounded-4">
@@ -102,25 +109,20 @@
                                             <div class="card-body ">
                                                 <h5 class="card-title text-truncate">{{ $article->title }}</h5>
                                                 <p class="card-text text-truncate">{{ $article->subtitle }}</p>
-                                                @if($article->category)
-
                                                 <a href="{{ route('article.byCategory', ['category' => $article->category->id]) }}"
-                                                    class="small text-muted  text-capitalize text-truncate"><b>Categoria:</b> {{ $article->category->name }}</a>
-                                                    @else
-                                                    <p class="small text-muted fst-italic text-capitalize">Non categorizzato</p>
-                                                    @endif
-                                                    <br>
-                                                    <p class="small fst-italic text-capitalize">
-                                                        @foreach ($article->tags as $tag)
-                                                            #{{ $tag->name }}
-                                                        @endforeach
-                                                    </p>
+                                                    class="small text-muted  text-capitalize text-truncate">{{ $article->category->name }}</a>
                                                 <a href="{{ route('article.byRedactor', ['user' => $article->user->id]) }}"
-                                                    class="small text-muted  text-capitalize text-truncate"><b>Redatto da:</b> 
+                                                    class="small text-muted  text-capitalize text-truncate">
                                                     {{ $article->user->name }}</a>
-                                                <br>
+                                                <p class="small text-muted  text-capitalize">
+                                                    {{ $article->category->name }}</p>
+                                                <div
+                                                    class="text-muted d-none d-flex justify-content-between align-items-center">
+                                                    Redatto il {{ $article->created_at->format('d/m/Y') }} da
+                                                    {{ $article->user->name }}
+                                                </div>
                                                 <a href="{{ route('article.show', compact('article')) }}"
-                                                    class="btn btn-secondary mt-2">Leggi</a>
+                                                    class="btn btn-secondary ">Leggi</a>
                                             </div>
                                         </div>
                                     </div>
@@ -129,10 +131,10 @@
                         </div>
                     </div>
                 </div>
-                <div class="carousel-item active" data-bs-interval="10000">
+                <div class="carousel-item">
                     <div class="cards-wrapper pb-5">
-                        <div class="container my-5">
-                            <div class="row justify-content-center">
+                        <div class="container  my-5">
+                            <div class="row  justify-content-around ">
                                 @foreach ($articles as $article)
                                     <div class="col-10 col-md-3 my-2 ">
                                         <div class="card  shadow  border border-3  rounded-4">
@@ -141,25 +143,20 @@
                                             <div class="card-body ">
                                                 <h5 class="card-title text-truncate">{{ $article->title }}</h5>
                                                 <p class="card-text text-truncate">{{ $article->subtitle }}</p>
-                                                @if($article->category)
-
                                                 <a href="{{ route('article.byCategory', ['category' => $article->category->id]) }}"
-                                                    class="small text-muted  text-capitalize text-truncate"><b>Categoria:</b> {{ $article->category->name }}</a>
-                                                    @else
-                                                    <p class="small text-muted fst-italic text-capitalize">Non categorizzato</p>
-                                                    @endif
-                                                    <br>
-                                                    <p class="small fst-italic text-capitalize">
-                                                        @foreach ($article->tags as $tag)
-                                                            #{{ $tag->name }}
-                                                        @endforeach
-                                                    </p>
+                                                    class="small text-muted  text-capitalize text-truncate">{{ $article->category->name }}</a>
                                                 <a href="{{ route('article.byRedactor', ['user' => $article->user->id]) }}"
-                                                    class="small text-muted  text-capitalize text-truncate"><b>Redatto da:</b> 
+                                                    class="small text-muted  text-capitalize text-truncate">
                                                     {{ $article->user->name }}</a>
-                                                <br>
+                                                <p class="small text-muted  text-capitalize">
+                                                    {{ $article->category->name }}</p>
+                                                <div
+                                                    class="text-muted d-none d-flex justify-content-between align-items-center">
+                                                    Redatto il {{ $article->created_at->format('d/m/Y') }} da
+                                                    {{ $article->user->name }}
+                                                </div>
                                                 <a href="{{ route('article.show', compact('article')) }}"
-                                                    class="btn btn-secondary mt-2">Leggi</a>
+                                                    class="btn btn-secondary ">Leggi</a>
                                             </div>
                                         </div>
                                     </div>
@@ -167,8 +164,9 @@
                             </div>
                         </div>
                     </div>
-                </div>
-            <button class="carousel-control-prev" type="button" data-bs-target="#carouselExampleInterval"
+                </div> --}}
+            </div>
+            {{-- <button class="carousel-control-prev" type="button" data-bs-target="#carouselExampleInterval"
                 data-bs-slide="prev">
                 <span class="carousel-control-prev-icon" aria-hidden="true"></span>
                 <span class="visually-hidden">Previous</span>
@@ -177,7 +175,7 @@
                 data-bs-slide="next">
                 <span class="carousel-control-next-icon" aria-hidden="true"></span>
                 <span class="visually-hidden">Next</span>
-            </button>
+            </button> --}}
         </div>
     </div>
 
--- resources/views/writer/dashboard.blade.php
+++ resources/views/writer/dashboard.blade.php
@@ -1,9 +1,9 @@
 <x-layout>
 
-    <div class=" container-fluid p-5 bg-info text-center text-white">
+    <div class=" container-fluid p-5  text-center ">
         <div class="row justify-content-center">
-            <h1 class="display-1">
-                Bentornato, Redattore
+            <h1 class="writerDashboard pt-5">
+                Bentornato
             </h1>
         </div>
     </div>
@@ -14,7 +14,7 @@
         </div>
     @endif
     
-    <div class=" container my-5">
+    <div class="writerBox1 p-3 container my-5">
         <div class="row justify-content-center">
             <div class="col-12">
                 <h2>Articoli in fase di revisione</h2>
@@ -23,7 +23,7 @@
         </div>
     </div>
     
-    <div class="container my-5">
+    <div class="writerBox2 p-3 container  my-5">
         <div class="row justify-content-center">
             <div class="col-12">
                 <h2>Articoli pubblicati</h2>
@@ -32,7 +32,7 @@
         </div>
     </div>
     
-    <div class="container my-5">
+    <div class="writerBox3 p-3 container my-5">
         <div class="row justify-content-center">
             <div class="col-12">
                 <h2>Articoli respinti</h2>
