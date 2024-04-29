a299002e7e06aa78d23bdb59ef8bdfd47e0b853a
lorenzoamosso <lorenzoamossoo@gmail.com>
2024-04-29 18:34:22 +0200

    none

--- resources/views/article/show.blade.php
+++ resources/views/article/show.blade.php
@@ -10,7 +10,7 @@
     
     <div class="container my-5 ">
         <div class="row justify-content-around margin">
-            <div class="col-11 col-md-6 col-lg-4">
+            <div class="col-11 col-md-6 col-lg-9">
             <div class="card img-thumbnail shadow  border border-3  rounded-4">
                 <img src="{{ Storage::url($article->img) }}" alt="" class="img-fluid my-3 border rounded-4 mt-1 ">
                 <div class="text-center ">
@@ -20,20 +20,21 @@
                     </div>
                 </div>
                     
+                
                     <p class="text-center p-4 ">{{ $article->body }}</p>
-                    <div class="d-flex justify-content-between ">
+                    <div class="d-flex justify-content-around p-3">
                         @if(Auth::user() && Auth::user()->is_revisor)
                         
                     <form action="{{route('revisor.acceptArticle', compact('article'))}}" method="POST">
                         @csrf
                         <div class="row ps-3">
-                        <button class="btn btn-success text-white p-2 m-3">Accetta  <i class="bi bi-check-lg"></i></button>
+                        <button class="btn btn-success text-white p-2 m-1">Accetta  <i class="bi bi-check-lg"></i></button>
                         </div>
                     </form>
                     <form action="{{route('revisor.rejectArticle', compact('article'))}}" method="POST">
                         <div class="row pe-5">
                         @csrf
-                        <button class="btn btn-danger text-white p-2 m-3">Rifiuta  <i class="bi bi-ban"></i></button>
+                        <button class="btn btn-danger text-white p-2  m-1">Rifiuta  <i class="bi bi-ban"></i></button>
                     </div>
                     </form>
                         
